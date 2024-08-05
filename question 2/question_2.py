a0b3703a1cf4b158aa147371bbbae05d6de14869
spyro254 <abdulrehmarnali@gmail.com>
2024-08-05 12:48:24 +0100

    Question 2 start

---
+++ question 2/question_2.py
@@ -0,0 +1,7 @@
+# Q2
+# Write code to read from a text file containing a list of numbers, each on a new line
+# The code should output the top five most frequent numbers, each on a new line - and nothing else
+#  
+
+import os
+
