bdb0bb134baab84693721c1fe3a88cda2a790040
spyro254 <abdulrehmarnali@gmail.com>
2024-08-05 14:10:10 +0100

    js code complete, just need to style

--- question 4/question_4.js
+++ question 4/question_4.js
@@ -11,7 +11,7 @@ const processFile = function() {
     if (data) {
         console.log('data loaded.....')
         const reader = new FileReader();
-        // append
+        // run after file has been loaded succesfully
         reader.onload = function(e) {
             // const content = e.target.result.split('\n').map(s => parseInt(s, 10));
             // convert data to array of numbers from string value
@@ -23,11 +23,24 @@ const processFile = function() {
                 else {frequencies[number] = 1}
             });
 
+            // sort numbers by frequency, and numerically if frequencies are the same
             const sortedNums = Object.keys(frequencies).sort((current, next) => {
-                
+                // if frequencies are the same
+                if (frequencies[next] === frequencies[current]) {
+                    return current - next;
+                }
+                return frequencies[next] - frequencies[current]
+            })
+
+            // display the 5 most frequent numbers
+            sortedNums.slice(0, 5).forEach(number => {
+                const item = document.createElement('li');
+                item.innerHTML = number;
+                results.appendChild(item)
             })
         }
-
+        // ensure reader is executed to load data as text
         reader.readAsText(data);
     }
+    else {alert("No file selected. Please use the 'Browse...' button.")}
 }
