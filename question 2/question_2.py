f7bc15549428015cd0a11ac3718ab01b30d17468
spyro254 <abdulrehmarnali@gmail.com>
2024-08-05 13:13:09 +0100

    Commit before fixing error line 12

--- question 2/question_2.py
+++ question 2/question_2.py
@@ -5,7 +5,7 @@
 
 import os
 
-# function to read numbers.txt file
+# function to read numbers.txt file, found in same working directory as .py file
 def readFile():
     f = open('numbers.txt', 'r')
     # splitlines func used to turn each new line into an array entry
@@ -22,4 +22,13 @@ def getMostFrequent(data):
             frequencies[number] += 1
         else:
             frequencies[number] = 1
-            
+
+    # sorted function puts values in ascending order
+    sorted_dict = sorted(frequencies, key=frequencies.get, reverse=True)
+    # print top 5 most frequent
+    for number in sorted_dict[:5]:
+        print(number)
+
+if __name__  == "__main__":
+    print("Current working directory:", os.getcwd())
+    getMostFrequent(readFile())
