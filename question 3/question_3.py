d018e6c329d618eec9a9774a5b6f1d9f0850cf93
spyro254 <abdulrehmarnali@gmail.com>
2024-08-05 17:06:38 +0100

    Question 3 batch process complete

--- question 3/question_3.py
+++ question 3/question_3.py
@@ -1,1 +1,31 @@
 import csv
+import os
+import sys
+
+py_dir = os.path.dirname(os.path.abspath(__file__))
+os.chdir(py_dir)
+
+data_path = 'NIFTY50_all.csv'
+batch_size = 1500
+
+def process_data(batch, i):
+            print(f"Processing batch {i} with {len(batch)} rows")
+            print("----- amazing stuff is happening with this data -----")
+
+def batch_processor(file_path, batch_size):
+    with open(file_path, 'r') as file:
+        reader = csv.reader(file)
+        header = next(reader, None)
+        current_batch = []
+        i = 1
+        for row in reader:
+            current_batch.append(row)
+            if len(current_batch) >= batch_size:
+                process_data(current_batch, i)
+                i += 1
+                current_batch = []
+        
+        if current_batch:
+                process_data(current_batch, i)
+
+batch_processor(data_path, batch_size)
